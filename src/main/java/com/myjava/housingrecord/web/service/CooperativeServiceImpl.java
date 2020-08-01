cbbd512cdf9ee2dd84127e1ee7f3cc97c23a7a4b
Tommi <tommi.korteikko@iCloud.com>
2020-08-01 23:06:08 +0300

    Added repository functionality

--- src/main/java/com/myjava/housingrecord/web/service/CooperativeServiceImpl.java
+++ src/main/java/com/myjava/housingrecord/web/service/CooperativeServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.myjava.housingrecord.domain.HousingCooperative;
 import com.myjava.housingrecord.repositories.CooperativeRepository;
 import com.myjava.housingrecord.web.mappers.CooperativeMapper;
 import com.myjava.housingrecord.web.model.HousingCooperativeDto;
@@ -19,7 +20,8 @@ public class CooperativeServiceImpl implements CooperativeService {
 	
 	public HousingCooperativeDto getById(UUID cooperativeId) {
 		
-		return mapper.HousingCooperativeToDto(repository.findById(cooperativeId).orElseThrow());
+		return mapper.HousingCooperativeToDto(
+				repository.findById(cooperativeId).orElseThrow());
 	}
 
 	@Override
@@ -30,14 +32,21 @@ public class CooperativeServiceImpl implements CooperativeService {
 
 	@Override
 	public HousingCooperativeDto updateHC(UUID cooperativeId, HousingCooperativeDto hcDto) {
-		// TODO Auto-generated method stub
-		return null;
+		HousingCooperative hc = repository.findById(cooperativeId).orElseThrow();
+		hc.setAddress(hcDto.getAddress());
+		hc.setBankAccount(hcDto.getBankAccount());
+		hc.setCity(hcDto.getCity());
+		hc.setName(hcDto.getName());
+		hc.setPostNumber(hcDto.getPostNumber());
+		
+		return mapper.HousingCooperativeToDto(repository.save(hc));
 	}
 
 	@Override
 	public HousingCooperativeDto saveNewHc(HousingCooperativeDto hcDto) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return mapper.HousingCooperativeToDto(
+				repository.save(mapper.DtoToHousingCooperative(hcDto)));
 	}
 
 }
