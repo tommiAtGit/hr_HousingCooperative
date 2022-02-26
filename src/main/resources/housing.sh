cfe46527e3deba3c966e0a13ec0df0d233845d90
Tommi <tommi.korteikko@iCloud.com>
2022-02-26 20:01:07 +0200

    Fixed service testcases

--- src/main/resources/housing.sh
+++ src/main/resources/housing.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mysqladmin -h localhost -P 3306 --protocol=tcp -u root --password=Tommijgqb#19 drop hc_housingdb
 mysqladmin -h localhost -P 3306 --protocol=tcp -u root --password=Tommijgqb#19 create hc_housingdb
-mysql -h localhost -P 3306 --protocol=tcp -u root --password=Tommijgqb#19 housingdb < ./HousingCooperative.sql
+mysql -h localhost -P 3306 --protocol=tcp -u root --password=Tommijgqb#19 hc_housingdb < ./HousingCooperative.sql
 mysqlshow -h localhost -P 3306 --protocol=tcp -u root --password=Tommijgqb#19
 mysqlshow -h localhost -P 3306 --protocol=tcp -u root --password=Tommijgqb#19 hc_housingdb
--- src/test/java/com/myjava/housingrecord/web/service/HousingRecordServiceTest.java
+++ src/test/java/com/myjava/housingrecord/web/service/HousingRecordServiceTest.java
@@ -38,7 +38,6 @@ class HousingRecordServiceTest {
 	void GetByIdTest() {
 		UUID ExpectedId = UUID.fromString("3132332d-3132-332d-3435-360000000000");
 		
-		
 		HousingCooperativeDto actualDto = service.getById(ExpectedId);
 		assertNotNull(actualDto);
 		
@@ -55,6 +54,37 @@ class HousingRecordServiceTest {
 		System.out.println("*** uuid: **** " + actualDto.getId().toString());
 		
 	}
+	
+	@Test
+	void UpdateHcTest() {
+		
+		String expectedAddress = "Some Test Address 1";
+		String expectedCity = "Expected city";
+		HousingCooperativeDto actualDto = service.saveNewHc(getValidCooperateiveDto());
+		assertNotNull(actualDto);
+		assertEquals(getValidCooperateiveDto().getName(), actualDto.getName());
+		
+		actualDto.setAddress(expectedAddress);
+		actualDto.setCity(expectedCity);
+		
+		HousingCooperativeDto updatedDto = service.updateHC(actualDto.getId(), actualDto);
+		
+		if (updatedDto == null) {
+			assertFalse(false);
+		}
+		
+		assertEquals(expectedAddress, updatedDto.getAddress());
+		assertEquals(expectedCity, updatedDto.getCity());
+		
+		
+		//Clear content
+		try {
+			service.deleteHc(updatedDto);
+		} catch (Exception e) {
+			assertFalse(false);
+		}
+	}
+	
 	
 	private HousingCooperativeDto getValidCooperateiveDto() {
 		
